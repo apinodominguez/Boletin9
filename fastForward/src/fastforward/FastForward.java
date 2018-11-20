c7ea042a6bb57bde9386caae0c0d78a15cafae79
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2018-11-20 09:18:52 +0100

    estupido merge. 2.2. featured

--- fastForward/src/fastforward/FastForward.java
+++ fastForward/src/fastforward/FastForward.java
@@ -5,6 +5,7 @@ public class FastForward {
     public static void main(String[] args) {
         // Creacion del fichero
         // Otro commit
+        // commit de la rama featured
     }
     
 }
